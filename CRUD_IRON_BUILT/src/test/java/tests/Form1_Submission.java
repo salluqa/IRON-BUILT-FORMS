65cbf49b8902d2ddc14d2e449ef6989fc9a317b3
Salman Ali <salman.ali@rolustech.com>
2024-08-16 19:02:23 +0500

    Form test

--- CRUD_IRON_BUILT/src/test/java/tests/Form1_Submission.java
+++ CRUD_IRON_BUILT/src/test/java/tests/Form1_Submission.java
@@ -21,7 +21,7 @@ public class Form1_Submission extends BaseTest {
 
 		try {
 			if (driver == null) {
-				test.log(Status.FAIL, "WebDriver is not initialized.");
+				test.log(Status.FAIL, "WebDriver is not initialized currently");
 				throw new NullPointerException("WebDriver is not initialized.");
 			}
 
